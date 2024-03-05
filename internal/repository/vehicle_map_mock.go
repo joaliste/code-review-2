017c0134b6c92110b735629c11723ca250d70920
Joaliste <jorgeignacio.aliste@mercadolibre.cl>
2024-03-05 19:22:25 -0300

    feature: find by color and year service method test

--- internal/repository/vehicle_map_mock.go
+++ internal/repository/vehicle_map_mock.go
@@ -13,3 +13,28 @@ type VehicleMapMock struct {
 		FindByColorAndYear int
 	}
 }
+
+func (v2 *VehicleMapMock) FindAll() (v map[int]internal.Vehicle, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (v2 *VehicleMapMock) FindByColorAndYear(color string, fabricationYear int) (v map[int]internal.Vehicle, err error) {
+	v2.Spy.FindByColorAndYear++
+	return v2.FindByColorAndYearFunc(color, fabricationYear)
+}
+
+func (v2 *VehicleMapMock) FindByBrandAndYearRange(brand string, startYear int, endYear int) (v map[int]internal.Vehicle, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (v2 *VehicleMapMock) FindByBrand(brand string) (v map[int]internal.Vehicle, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (v2 *VehicleMapMock) FindByWeightRange(fromWeight float64, toWeight float64) (v map[int]internal.Vehicle, err error) {
+	//TODO implement me
+	panic("implement me")
+}
--- internal/service/vehicle_default_test.go
+++ internal/service/vehicle_default_test.go
@@ -1,7 +1,65 @@
 package service_test
 
-import "testing"
+import (
+	"app/internal"
+	"app/internal/repository"
+	"app/internal/service"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestServiceVehicleDefault_FindByColorAndYear(t *testing.T) {
+	// Given
+	rp := repository.NewVehicleMapMock()
+	rp.FindByColorAndYearFunc = func(color string, fabricationYear int) (v map[int]internal.Vehicle, err error) {
+		return map[int]internal.Vehicle{1: {
+			Id: 1,
+			VehicleAttributes: internal.VehicleAttributes{
+				Brand:           "A",
+				Model:           "B",
+				Registration:    "C",
+				Color:           "D",
+				FabricationYear: 1,
+				Capacity:        1,
+				MaxSpeed:        1,
+				FuelType:        "E",
+				Transmission:    "F",
+				Weight:          1,
+				Dimensions: internal.Dimensions{
+					Height: 1,
+					Length: 1,
+					Width:  1,
+				},
+			},
+		}}, nil
+	}
+	sv := service.NewServiceVehicleDefault(rp)
+
+	expectedResult := map[int]internal.Vehicle{1: {
+		Id: 1,
+		VehicleAttributes: internal.VehicleAttributes{
+			Brand:           "A",
+			Model:           "B",
+			Registration:    "C",
+			Color:           "D",
+			FabricationYear: 1,
+			Capacity:        1,
+			MaxSpeed:        1,
+			FuelType:        "E",
+			Transmission:    "F",
+			Weight:          1,
+			Dimensions: internal.Dimensions{
+				Height: 1,
+				Length: 1,
+				Width:  1,
+			},
+		},
+	}}
+	// When
+	result, err := sv.FindByColorAndYear("D", 1)
+	// Assert
+	assert.Nil(t, err)
+	assert.Equal(t, expectedResult, result)
+	assert.Equal(t, 1, rp.Spy.FindByColorAndYear)
 
 }
